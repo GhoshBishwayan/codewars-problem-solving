90df16e0b9ef3b3bd1257237dc06034fe3fb08d5
GhoshBishwayan <ghoshbishwayan@gmail.com>
2026-02-01 00:13:33 +0530

    first commit

---
+++ problem7.py
@@ -0,0 +1,60 @@
+'''Some numbers have funny properties. For example:
+
+89 --> 8¹ + 9² = 89 * 1
+695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+Given two positive integers n and p, we want to find a positive integer k, if it exists, such that the sum of the digits of n raised to consecutive powers starting from p is equal to k * n.
+
+In other words, writing the consecutive digits of n as a, b, c, d ..., is there an integer k such that :
+
+(a^p+b^(p+1)+c^(p+2)+d^(p+3)+...)=n∗k
+
+If it is the case we will return k, if not return -1.
+
+Note: n and p will always be strictly positive integers.
+
+Examples:
+n = 89; p = 1 ---> 1 since 8¹ + 9² = 89 = 89 * 1
+
+n = 92; p = 1 ---> -1 since there is no k such that 9¹ + 2² equals 92 * k
+
+n = 695; p = 2 ---> 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+n = 46288; p = 3 ---> 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+'''
+# My solution
+def dig_pow(n, p):
+    # your code
+    sum = 0
+    r = 0
+    for i in str(n):
+        sum = sum + int(i)**(p+r)
+        r+=1
+    x = sum/n
+    if x%1 == 0:
+        return x
+    return -1
+
+# sol2 Best imo
+def dig_pow(n, p):
+    sum = 0
+    for c in str(n):
+        sum += int(c) ** p
+        p += 1
+    if sum % n == 0:
+        return sum / n
+    else:
+        return -1
+
+# others sol1
+def dig_pow(n, p):
+  s = 0
+  for i,c in enumerate(str(n)):
+     s += pow(int(c),p+i)
+  return s/n if s%n==0 else -1
+
+# short
+def dig_pow(n, p):
+    k, fail = divmod(sum(int(d)**(p + i) for i, d in enumerate(str(n))), n)
+    return -1 if fail else k
